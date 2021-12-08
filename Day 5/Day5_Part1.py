e37055a86473f8599bdc3a954df77727800df925
Logan Michaels <95458200+ldmichae@users.noreply.github.com>
2021-12-07 23:23:12 -0500

    Update Day5_Part1.py

--- Day 5/Day5_Part1.py
+++ Day 5/Day5_Part1.py
@@ -13,12 +13,14 @@ def gen_line(start, end):
     calcd_points = []
     calcd_points.append(start)
     if start[0] == end[0]:   ##horizontal line
-        print('horiz')
-        for i in range(start[1] + 1, end[1]):
+        p1 = start[1]
+        p2 = end[1]
+        for i in range(min(p1,p2) + 1, max(p1,p2)):
             calcd_points.append([start[0], i])
     else:                    ##vertical line
-        print('vert')
-        for i in range(start[0] + 1, end[0]):
+        p1 = start[0]
+        p2 = end[0]
+        for i in range(min(p1,p2) + 1, max(p1,p2)):
             calcd_points.append([i, end[1]])
     calcd_points.append(end)
     return calcd_points
@@ -34,11 +36,15 @@ for reading in readings:
 point_counts = {}
 for valid_reading in valid_readings:
     points_crossed = gen_line(valid_reading[0],valid_reading[1])
-    print(points_crossed)
     for point in range(len(points_crossed)):
         point = points_crossed[point]
-        if point not in list(point_counts.keys()):
+        if str(point) not in list(point_counts.keys()):
             point_counts[str(point)] = 1
         else:
             point_counts[str(point)] += 1
+# %%
+result = 0
+for i,j in point_counts.items():
+    if j > 1:
+        result +=1
 # %%
